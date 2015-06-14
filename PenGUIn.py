a673cb2b4cce514f93441cfce5e707944a60a626
Bnesngo <Bnesngo@users.noreply.github.com>
2015-06-13 19:31:35 -0700

    Create PenGUIn.py
    This is in Python mainly because I don't think js goes to gui.

---
+++ PenGUIn.py
@@ -0,0 +1,12 @@
+import tkinter
+log = []
+
+class root: #this is basic. Destroy and create.
+  def __init__():
+    pass
+  def destroy():
+    self = None
+  def create():
+    log.append(self)
+  
+  
