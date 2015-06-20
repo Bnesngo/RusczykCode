ce11aed19b77a218dd363cdb8388e0433b7c1009
Bnesngo <Bnesngo@users.noreply.github.com>
2015-06-19 17:50:32 -0700

    Added operators

--- Operators.js
+++ Operators.js
@@ -1,1 +1,21 @@
+add = function(x, y) {
+  return x + y;
+}
 
+subtract = function(x, y) {
+  return x - y;
+}
+
+multiply = function(x, y) {
+  return x * y;
+}
+
+divide = function(x, y) {
+  return x / y;
+}
+
+mod = function(x, y) {
+  return x % y;
+}
+
+//Basic Operators
