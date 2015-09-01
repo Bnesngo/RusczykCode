4d326596d9226fecb80f442d78351c30c1704d60
h313 <h313@users.noreply.github.com>
2015-09-01 09:14:39 -0400

    Created py file for data structures
    So far, has queue, stack, bag, and base DS class.

---
+++ DataStructures.py
@@ -0,0 +1,32 @@
+class baseDS:
+    _data = []
+    count = 0
+    def get(self, location):
+        return data[location]
+    def sort(self):
+        data.sort()
+    def find(self, i):
+        try:
+            return data.index(i)
+        except:
+            return -1
+
+class Queue(baseDS):
+    def addToBack(self, item):
+        data.append(item)
+        count = count + 1
+    def removeFromFront(self):
+        data.remove(data[0])
+        count = count + 1
+
+class Bag(baseDS):
+    def add(self, item):
+        data.append(item)
+        count = count + 1       
+
+class Stack(baseDS):
+    def addToTop(self, item):
+        data.append(item)
+        count = count + 1
+    def removeFromTop(self):
+        data.remove(data[count-1])
